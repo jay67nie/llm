437409d89768465ccea6f9716cd3c7a6b3a1d155
Jerome Ssenyonjo <jeromessenyonjo@gmail.com>
2024-05-15 23:46:36 +0300

    Chunked education guide

--- chunk.py
+++ chunk.py
@@ -5,12 +5,6 @@ from langchain_community.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
-with open("text/construction.txt", "r", encoding='utf-8') as f:
-    construction_txt = f.read()
-
-with open("text/agric.txt", "r", encoding='utf-8') as f:
-    agric_text = f.read()
-
 
 load_dotenv()
 
@@ -18,15 +12,29 @@ os.environ['OPENAI_API_KEY'] = os.environ.get("OPENAI_API_KEY")
 
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
 
-text_chunks = text_splitter.create_documents([construction_txt])
+# with open("text/construction.txt", "r", encoding='utf-8') as f:
+#     construction_txt = f.read()
+#
+# text_chunks = text_splitter.create_documents([construction_txt])
+#
+# db = Chroma.from_documents(documents=text_chunks, embedding=OpenAIEmbeddings(), collection_name="const_guide",
+#                            persist_directory="./chroma_db")
+#
+# with open("text/agric.txt", "r", encoding='utf-8') as f:
+#     agric_text = f.read()
+#
+# text_chunks = text_splitter.create_documents([agric_text])
+#
+# db = Chroma.from_documents(documents=text_chunks, embedding=OpenAIEmbeddings(), collection_name="agric_guide",
+#                            persist_directory="./chroma_db")
 
-db = Chroma.from_documents(documents=text_chunks, embedding=OpenAIEmbeddings(), collection_name="const_guide",
-                           persist_directory="./chroma_db")
+with open("text/education.txt", "r", encoding='utf-8') as f:
+    education_txt = f.read()
 
-text_chunks = text_splitter.create_documents([agric_text])
+text_chunks = text_splitter.create_documents([education_txt])
 
-db = Chroma.from_documents(documents=text_chunks, embedding=OpenAIEmbeddings(), collection_name="agric_guide",
-                           persist_directory="./chroma_db")
+db = Chroma.from_documents(documents=text_chunks, embedding=OpenAIEmbeddings(), collection_name="education_guide")
+
 
 print("Done")
 
