3e00a56be651b713e2059d267cb3e0fd486c9787
Samson Wanendeya <nateniel18@gmail.com>
2024-05-20 23:46:07 +0300

    added chat history to store messages

--- UI.py
+++ UI.py
@@ -5,10 +5,19 @@ import streamlit as st
 
 def handle_user_question(user_question):
     print(user_question)
+    st.write(st.session_state.messages)
+    st.session_state.messages.append({"role":"user", "content":user_question})
+    st.session_state.messages.append({"role":"assistant","content": "This is the response to the user question"})
+    
 
     
 
 def main():
+     # Initialize session state for chat messages
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+   
     hide_decoration_bar_style = '''
         <style>
             header {visibility: hidden;}
@@ -16,10 +25,19 @@ def main():
         '''
     st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
-    user_question = st.chat_input("Ask Question here")
 
-    if user_question:
+    if user_question := st.chat_input("Ask Question here"):
         handle_user_question(user_question)
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+
+
+    
+
+
     with st.sidebar:
         st.write("This is sidebar that will contain the chat history")
 
