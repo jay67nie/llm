31ad456dead464fa7f5273dbf1c51be3e2aa7458
Jerome Ssenyonjo <jeromessenyonjo@gmail.com>
2024-06-16 19:18:15 +0300

    "Modularized functionality"

--- chat_with_openai_assistant_evaluate.py
+++ chat_with_openai_assistant_evaluate.py
@@ -1,6 +1,5 @@
 import json
 import os
-
 from dotenv import load_dotenv
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import FlashrankRerank
@@ -11,21 +10,6 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from openai import OpenAI
 from openai.lib.streaming import AssistantEventHandler
 from typing_extensions import override
-
-# TODO: Add streaming support for the chat response. (Jerome) ✔️
-# TODO: Create a new thread for each user, and delete it when the chat is done. (Samson)
-# TODO: Modify the functionality to suit frontend, but maintain the backend logic. (Samson)
-# TODO: Suggest a persona for the user to use when chatting with the assistant. (Jerome) ✔️
-# TODO: Evaluate the responses from the assistant. (Jerome)
-chat_history = []
-
-contextualize_q_system_prompt = """Given a chat history and the latest user question \
-    which might reference context in the chat history, formulate a standalone question as the user persona \
-    which can be understood by the document retriever without the chat history. Do NOT answer the question! Strictly \
-    just restructure it as a question for the retriever. I repeat, do not answer the user question!!!!
-    Only restructure the question!!!"""
-
-chat_history.append(('system', contextualize_q_system_prompt))
 
 load_dotenv()
 os.environ['OPENAI_API_KEY'] = os.environ.get("OPENAI_API_KEY")
@@ -53,18 +37,9 @@ class EventHandler(AssistantEventHandler):
                     if output.type == "logs":
                         print(f"\n{output.logs}", flush=True)
 
-client = None
-retriever = None
-compression_retriever = None
-openai_client = OpenAI()
-thread_id = None
-sector = None
-# Retrieve from an existing collection
 
 def set_sector(selected_sector):
-# sector = input("Enter the sector: ")
-    global db
-    global client, retriever, compression_retriever, thread_id, sector
+    global db, client, retriever, compression_retriever, openai_client, thread_id, sector
     sector_mapping = {
         "agriculture": "agric",
         "construction": "const",
@@ -79,50 +54,87 @@ def set_sector(selected_sector):
 
     if sector_key:
         sector = sector_mapping[sector_key]
-
         print(f"Setting sector to {sector}")
-        db = Chroma(persist_directory="./chroma_db", collection_name=f"{sector}_guide", embedding_function=OpenAIEmbeddings())
+        db = Chroma(persist_directory="./chroma_db", collection_name=f"{sector}_guide",
+                    embedding_function=OpenAIEmbeddings())
         retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 3})
         compressor = FlashrankRerank(top_n=3)
         compression_retriever = ContextualCompressionRetriever(base_retriever=retriever, base_compressor=compressor)
         openai_client = OpenAI()
-        thread_id = openai_client.beta.threads.create().id
+        thread_id = "thread_AYA7B1cxHaA1Pbl08Gt5H62M"
     else:
         raise ValueError("Invalid sector provided")
-    
-    
-     
-
-    # client = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
-    
-
-    # thread_id = create_thread()  # Call create_thread() to create a new thread
 
 
-    # TODO: Print thread to file called thread.txt
-    # with open(f"{sector}_thread.txt", "w") as f:
-    #     f.write(thread.id)
+def initialize_database(sector):
+    db = Chroma(persist_directory="./chroma_db", collection_name=f"{sector}_guide",
+                embedding_function=OpenAIEmbeddings())
+    retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 3})
+    compressor = FlashrankRerank(top_n=3)
+    compression_retriever = ContextualCompressionRetriever(base_retriever=retriever, base_compressor=compressor)
+    return compression_retriever
+
+
+def create_thread():
+    openai_client = OpenAI()
+    thread = openai_client.beta.threads.create()
+
+    thread_id = thread.id
+
+    with open("thread.txt", "w") as file:
+        file.write(thread_id)
+
+    return thread_id
+
+
+def delete_thread(t_id):
+    openai_client.beta.threads.delete(thread_id=t_id)
+
+
+def create_system_prompt(sector):
+    return f"""The user is posing as a person in the {sector} sector who is a taxpayer. So the person in the {sector} 
+    sector, in the user question should be substituted for taxpayer in the passed-in context """
+
+
+def contextualize_query_for_retriever(query, chat_history):
+    local_chat_history = chat_history + [("user", "{question}")]
+    chat_prompt = ChatPromptTemplate.from_messages(local_chat_history)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2)
+    chain = chat_prompt | llm | StrOutputParser() | (lambda x: x.split("\n"))
+    llm_response = chain.invoke({"question": query})
+    print("LLM Response: ", llm_response)
+    return llm_response[0]
+
+
+chat_history = []
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question as the user persona \
+    which can be understood by the document retriever without the chat history. Do NOT answer the question! Strictly \
+    just restructure it as a question for the retriever. I repeat, do not answer the user question!!!! \
+    Only restructure the question!!!"""
+chat_history.append(('system', contextualize_q_system_prompt))
+
+
+def build_chat_history(query, response):
+    chat_history.append(('user', query))
+    chat_history.append(('assistant', response))
+    print("\nChat History: ", chat_history)
 
 
 def chat_with_assistant(query):
-    # Make the retriever history aware
-    # query = input('Enter your query: ')
     global db, thread_id, sector, compression_retriever, retriever, openai_client
-    
+
     print("##  Sector: ## ", sector)
     if db is None:
         raise ValueError("Database has not been set. Please set the sector first.")
-    
-   
 
     if len(chat_history) > 1:
-        contextualized_query = contextualize_query_for_retriever(query)
+        contextualized_query = contextualize_query_for_retriever(query, chat_history)
     else:
         contextualized_query = query
 
     reranked_results = compression_retriever.get_relevant_documents(contextualized_query)
-    
-    print("COMPRESSOR 2 RESULTS ", compression_retriever)
+    # print("COMPRESSOR 2 RESULTS ", compression_retriever)
 
     for result in reranked_results:
         print("Result", result, "\n")
@@ -136,7 +148,6 @@ def chat_with_assistant(query):
     {query}
     </question>
     """
-
     print("User message: ", user_message)
 
     openai_client.beta.threads.messages.create(
@@ -150,13 +161,6 @@ def chat_with_assistant(query):
         role="user",
         content=user_message
     )
-    # print(messages)
-
-    # run = openai_client.beta.threads.runs.create_and_poll(
-    #     thread_id=thread_id,
-    #     assistant_id=os.environ.get("ASSISTANT_ID"),
-    #     temperature=0
-    # )
 
     with openai_client.beta.threads.runs.stream(
             thread_id=thread_id,
@@ -165,73 +169,11 @@ def chat_with_assistant(query):
             event_handler=EventHandler()
     ) as stream:
         stream.until_done()
-
-        # if run.status == "completed":
         messages = openai_client.beta.threads.messages.list(thread_id=thread_id, limit=1, order="desc")
-
         msg_json = json.loads(messages.to_json())
-        # print(msg_json)
         if msg_json["data"][0]["role"] == "assistant":
             response = msg_json["data"][0]["content"][0]["text"]["value"]
             build_chat_history(query, response)
             return response
         else:
-             return "Assistant response not found"
-
-
-    # else:
-    #     print(run.status)
-
-
-def build_chat_history(query, response):
-    # print("GPT-35 Response: ", response)
-
-    chat_history.append(('user', query))
-    chat_history.append(('assistant', response))
-
-    print("\nChat History: ", chat_history)
-
-
-def contextualize_query_for_retriever(query):
-    local_chat_history = chat_history + [("user", "{question}")]
-    chat_prompt = ChatPromptTemplate.from_messages(
-        local_chat_history,
-    )
-
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.2)
-
-    chain = chat_prompt | llm | StrOutputParser() | (lambda x: x.split("\n"))
-
-    llm_response = chain.invoke({"question": query})
-
-    print("LLM Response: ", llm_response)
-
-    return llm_response[0]
-
-
-# Call when user is done chatting / leaves the website
-def delete_thread(t_id):
-    openai_client.beta.threads.delete(thread_id=t_id)
-
-
-# Call to create a new thread
-def create_thread():
-    thread = openai_client.beta.threads.create()
-    return thread.id
-
-
-def create_system_prompt(sector):
-    return f"""The user is posing as a person in the {sector} sector who is a taxpayer. So the person in the  {sector} 
-    sector, in the user question
-    should be substituted for taxpayer in the passed-in context """
-
-
-# while True:
-#     chat_with_assistant()
-#     user_input = input("Do you want to continue chatting? (yes/no): ")
-#     if user_input.lower() == "no":
-#         delete_thread(thread_id)
-#         break
-#     else:
-#         continue
-
+            return "Assistant response not found"
